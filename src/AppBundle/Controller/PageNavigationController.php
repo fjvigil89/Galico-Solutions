4305782063a5bcf0a52495ab67d355be03863e2c
jrhodelyr <jrhodelyr@gmail.com>
2017-05-02 06:27:12 -0400

    Les templates twigs (les pages html.twig) ajoutes ainsi que le controleur PageNavigationController pour la navigation entre les pages

---
+++ src/AppBundle/Controller/PageNavigationController.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rhood
+ * Date: 2017-05-01
+ * Time: 11:27 PM
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class PageNavigationController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function showHomeAction()
+    {
+        return $this->render('website/home.html.twig');
+    }
+
+    /**
+     * @Route("/services")
+     */
+    public function showServicesAction()
+    {
+        return $this->render('website/services-list.html.twig');
+    }
+
+    /**
+     * @Route("/plans")
+     */
+    public function showPlansAction()
+    {
+        return $this->render('website/plans.html.twig');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function showContactUsAction()
+    {
+        return $this->render('website/contact.html.twig');
+    }
+
+    /**
+     * @Route("/about")
+     */
+    public function showAboutUsAction()
+    {
+        return $this->render('website/about.html.twig');
+    }
+
+    /**
+     * @Route("/signin")
+     */
+    public function showSigninAction()
+    {
+        return $this->render('website/signin.html.twig');
+    }
+
+    /**
+     * @Route("/register")
+     */
+    public function showRegisterAction()
+    {
+        return $this->render('website/register.html.twig');
+    }
+}
