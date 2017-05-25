a575a82c796a2b916db53fd3e0235767c3a93154
Antonifuchong <antofuchong@gmail.com>
2017-05-25 17:26:08 -0400

    modifier text toutes les pages, ajouter des images, contrains pour registrer, contact us, etc

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ANTONIO
+ * Date: 2017-05-23
+ * Time: 21:22
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/sendEmail", name="send_email")
+     */
+    public function SendEmailAction(Request $request)
+    {
+        $cname = $request->query->get('cname');
+        $phone = $request->query->get('phone');
+        $email = $request->query->get('email');
+        $message = $request->query->get('message');
+
+
+
+        $messagesend = \Swift_Message::newInstance()
+            ->setSubject('cname')
+            ->setFrom('email')
+            ->setTo('antofuchong@gmail.com')
+            ->setBody('message');
+
+        $this->get('mailer')->send($messagesend);
+
+
+        die($cname.$email.$message);
+
+        return $this->render('website/plans.html.twig');
+    }
+}
