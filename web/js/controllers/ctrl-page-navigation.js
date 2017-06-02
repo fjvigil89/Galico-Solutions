04dd820b70ad9c0fc0d2ad7806fe509cd73ba873
Antonifuchong <antofuchong@gmail.com>
2017-06-01 21:43:49 -0400

    traduction de la page dashboard et qlq changement de textes

--- web/js/controllers/ctrl-page-navigation.js
+++ web/js/controllers/ctrl-page-navigation.js
@@ -6,8 +6,25 @@ angular.module("gpApp")
     .controller('PageNavigationController',function($scope,$rootScope,authService,GeneralService,$window){
 
         $scope.plan = {};
-        $scope.plan.name ="All Services";
+        $scope.plan.name = $("#allPlans").val();
         $scope.plan.text ="";
 
-
+        $scope.setPlanInfo = function(plan)
+        {
+            if(plan=='basic')
+            {
+                $scope.plan.name = "GP Basic";
+                $scope.plan.text = $("#GPBasic").val();
+            }
+            else if(plan=='plus')
+            {
+                $scope.plan.name = "GP Plus";
+                $scope.plan.text = $("#GPPlus").val();
+            }
+            else if(plan=='add')
+            {
+                $scope.plan.name = "GP ADD";
+                $scope.plan.text = $("#GPAdd").val();
+            }
+        }
     })
